880ea2d364272afb8cfdf44c28120f709d1eb233
Yusuf Salman Noerinaldy Siregar <yusufsiregar5@gmail.com>
2018-06-27 22:54:41 +0700

    finished

--- controllers.js
+++ controllers.js
@@ -6,10 +6,10 @@ const timer = seconds =>
     const then = now + seconds * 1000;
 
     displayCountdown(seconds - 1);
-    setInterval(() => {
+    let countdown = setInterval(() => {
       const secondsLeft = Math.floor((then - Date.now()) / 1000);
       if (secondsLeft < 0) {
-        clearInterval();
+        clearInterval(countdown)
         resolve();
       } else {
         displayCountdown(secondsLeft);
--- views.js
+++ views.js
@@ -5,7 +5,35 @@ const chalk = require("chalk");
 const displayCountdown = seconds => {
   clear();
   console.log(chalk.cyan(figlet.textSync("Countdown App")));
-  // Your code here...
+  if (seconds >= 60) {
+    let minuteDisplay = seconds / 60;
+    let strSecondsDisplay = null;
+    let strMinuteDisplay = null;
+    if (minuteDisplay >= 10) {
+      strMinuteDisplay = Math.trunc(minuteDisplay).toString();
+    } else {
+      strMinuteDisplay = "0" + Math.trunc(minuteDisplay).toString();
+    }
+    let secondsDisplay = seconds % 60;
+    if (secondsDisplay === 0) {
+      strSecondsDisplay = "00";
+    } else {
+      if (secondsDisplay >= 10) {
+        strSecondsDisplay = secondsDisplay.toString();
+      } else {
+        strSecondsDisplay = "0" + secondsDisplay.toString();
+      }
+    }
+    display = strMinuteDisplay + ":" + strSecondsDisplay;
+  } else {
+    let secondsDisplay = seconds.toString();
+    if (secondsDisplay.length === 1) {
+      display = "00:0" + secondsDisplay
+    } else {
+      display = "00:" + secondsDisplay
+    }
+  }
+  console.log(chalk.cyan(figlet.textSync(display)));
 };
 
 module.exports = { displayCountdown };
