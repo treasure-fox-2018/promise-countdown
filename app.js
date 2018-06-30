8d24f0185c3f6dd4a911633c4bcd279bc8ec5788
Your Name <fahrisyany@gmail.com>
2018-06-30 23:33:20 +0700

    done

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const { timer } = require("./controllers");
 
 const main = () => {
-  const seconds = parseInt(process.argv[2]) || 10;
+  const seconds = parseInt(process.argv[2]) || 100;
+  seconds
+  timer(seconds)
+
   // Your code here...
 };
 
 main();
+module.exports = main
--- views.js
+++ views.js
@@ -4,8 +4,13 @@ const chalk = require("chalk");
 
 const displayCountdown = seconds => {
   clear();
-  console.log(chalk.cyan(figlet.textSync("Countdown App")));
+  console.log(chalk.red(figlet.textSync("Countdown App")));
   // Your code here...
+  
+  let _minute = Math.floor(seconds / 60) % 10
+  let _second = Math.floor(seconds % 60) / 10
+  let time = `${_minute} : ${_second}`
+  console.log(chalk.red(figlet.textSync(time)));
 };
 
 module.exports = { displayCountdown };
